f3c5ffcde57e4760e7721305575232e2247f3aea
Hachicha Firas <firas.hachicha@esprit.tn>
2023-02-10 10:08:40 +0100

    first commit

---
+++ CRUDApplication/src/Config/Connexionbd.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Config;
+import java.sql.*;
+
+/**
+ *
+ * @author Asus
+ */
+public class Connexionbd {
+    
+    
+      private   Connection cnx ;
+        String url = "jdbc:mysql://localhost:3306/pidev3a26";
+       String user ="root";
+  String password ="";
+  public static Connexionbd ct;
+
+    private Connexionbd()  {
+        try {
+    cnx = DriverManager.getConnection(url, user, password);
+                System.out.println("cnx sucess");
+
+        } catch (Exception e) {
+            
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static Connexionbd getInstance(){
+        
+        if(ct == null)
+            ct = new Connexionbd();
+         return ct;
+        
+    }
+    
+    public Connection getCnx() {
+        return cnx;
+    }
+    
+    
+     
+      
+            
+        }
+        
+        
+
---
+++ CRUDApplication/src/Entity/Message.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Asus
+ */
+public class Message {
+    
+    private Freelancer source;
+    private Freelancer destinataire;
+    private LocalDateTime date;
+    private String contenu;
+    
+    
+    
+    
+}
